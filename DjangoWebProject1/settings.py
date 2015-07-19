786d2f4df9d77d3f684ccee7a768d18d7baf1721
unknown <ahmad.elsallab@gmail.com>
2015-07-19 15:12:10 +0200

    Stock pie and col charts added. Timezone fixed to be Etc/UTC in settings.py. The visual timezone is UTC not Saudi yet

--- DjangoWebProject1/settings.py
+++ DjangoWebProject1/settings.py
@@ -79,7 +79,7 @@ CRONJOBS = [
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = None
+TIME_ZONE = 'Etc/UTC'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
